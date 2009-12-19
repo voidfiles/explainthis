2ab963e07e9b700753d0b18ea97d596609716731
alex <alex@webdev.hsd1.ca.comcast.net>
2009-12-18 23:16:59 -0800

    added new menu

--- drupal-6.14/sites/explainthis.tastestalkr.com/themes/yui_grid/node-page.tpl.php
+++ drupal-6.14/sites/explainthis.tastestalkr.com/themes/yui_grid/node-page.tpl.php
@@ -5,7 +5,8 @@
 ?>
   <div class="node<?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?>">
     <?php if (1 == 0) { ?><h2 class="title"><a href="<?php print $node_url?>"><?php print $title?></a></h2><?php }; ?>
-    
+    <?php print $vote_storylink_via ?>
+    <?php print $vote_up_down_widget ?>
     <?php #print theme('imagecache', "1_3", $node->field_images[0]["filename"], "", "", ""); ?>
     <div class="content"><?php print $node->content["body"]["#value"]; ?></div>
   </div>
--- drupal-6.14/sites/explainthis.tastestalkr.com/themes/yui_grid/template.php
+++ drupal-6.14/sites/explainthis.tastestalkr.com/themes/yui_grid/template.php
@@ -32,6 +32,27 @@ if (is_null(theme_get_setting('yui_page_width'))) {
 }
 
 
+function _phptemplate_variables($hook, $vars) {
+  switch ($hook) {
+    case 'node':
+      $vars['storylink_url'] = check_url($vars['node']->vote_storylink_url);
+      if (arg(1) != 'add' && arg(2) != 'edit') {
+        $style = variable_get('vote_up_down_widget_style_node', 0) == 1 ? '_alt' : '';
+        $vars['vote_up_down_widget'] = theme("vote_up_down_widget$style", $vars['node']->nid, 'node');
+        $vars['vote_up_down_points'] = theme("vote_up_down_points$style", $vars['node']->nid, 'node');
+      }
+      $vars['vote_storylink_via'] = theme('vote_storylink_via', $vars['node']->vote_storylink_url);
+      if (arg(1) == 'top') {
+        static $count;
+        $count = is_array($count) ? $count : array();
+        $count[$hook] = is_int($count[$hook]) ? $count[$hook] : 1;
+        $vars['seqid'] = $count[$hook]++;
+      }
+      break;
+  }
+  return $vars;
+}
+
 function hook_form_node_alter(&$form, $form_state, $form_id){
     print "made it to the form";
 
